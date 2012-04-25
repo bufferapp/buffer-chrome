da557a295f6d762eeaadf54cfeb0fe558e4b32d8
Joel Gascoigne <me@joel.is>
2012-04-25 20:44:16 +0800

    "add to buffer" as a nice compromise on HN, I think that's perfect now.

--- chrome/data/buffer-hn.js
+++ chrome/data/buffer-hn.js
@@ -4,7 +4,7 @@ $(function() {
     config.base = "http://news.ycombinator.org/",
 	config.buttons = [
 	    {
-	        text: "buffer",
+	        text: "add to buffer",
     	    container: 'td.subtext',
 	        className: 'buffer-hn-button',
 	        data: function (elem) {
