257486722e20b4014c34e66afd16bb974bdb3087
Tom Ashworth <tom@phuu.net>
2012-05-11 14:28:02 +0100

    Update Chrome to accept statistics

--- chrome/data/main.js
+++ chrome/data/main.js
@@ -11,6 +11,8 @@ Authors: Joel Gascoigne         Tom Ashworth
 var config = {};
 config.plugin = {
     label: "Buffer This Page",
+    version: "2.1.3",
+    placement_prefix: 'chrome-',
     guide: 'http://bufferapp.com/guides/chrome/installed',
     menu: {
         page: {
@@ -31,6 +33,7 @@ var attachOverlay = function (data, cb) {
     if( typeof data === 'function' ) cb = data;
     if( ! data ) data = {};
     if( ! cb ) cb = function () {};
+    if( ! data.embed ) data.embed = {};
     
     var tab = data.tab;
         
@@ -45,6 +48,13 @@ var attachOverlay = function (data, cb) {
         }, 0);
     });
     
+    // Don't try to JSON encode a tab
+    data.tab = null;
+    // Pass statistic data
+    data.version = config.plugin.version;
+    if( data.embed.placement ) data.embed.placement = config.plugin.placement_prefix + data.embed.placement;
+    if( data.placement ) data.placement = config.plugin.placement_prefix + data.placement;
+    else if( data.embed.placement ) data.placement = data.embed.placement;
     // Inform overlay that click has occurred
     port.emit("buffer_click", data);
     
@@ -60,7 +70,7 @@ if( ! localStorage.getItem('buffer.run') ) {
 
 // Fire the overlay when the button is clicked
 chrome.browserAction.onClicked.addListener(function(tab) {
-    attachOverlay({tab: tab});
+    attachOverlay({tab: tab, placement: 'toolbar'});
 });
 
 // Context menus
@@ -69,7 +79,7 @@ chrome.contextMenus.create({
     title: config.plugin.menu.page.label,
     contexts: ["page"],
     onclick: function (info, tab) {
-        attachOverlay({tab: tab});
+        attachOverlay({tab: tab, placement: 'menu-page'});
     }
 });
 
@@ -78,7 +88,7 @@ chrome.contextMenus.create({
     title: config.plugin.menu.selection.label,
     contexts: ["selection"],
     onclick: function (info, tab) {
-        attachOverlay({tab: tab});
+        attachOverlay({tab: tab, placement: 'menu-selection'});
     }
 });
 
