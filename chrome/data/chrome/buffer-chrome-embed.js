f6064fb99ba2acf13aaffe15e3f273e27a002d88
Tom Ashworth <tom@phuu.net>
2012-06-22 17:09:35 +0100

    Oops, left in a console.log.

--- chrome/data/chrome/buffer-chrome-embed.js
+++ chrome/data/chrome/buffer-chrome-embed.js
@@ -2,6 +2,5 @@ if( ! xt.port.raw ) {
   xt.port = PortWrapper(chrome.extension.connect({name: "buffer-embed"}));
   xt.port.on('buffer_options', function (options) {
     xt.options = options;
-    console.log(xt.options);
   });
 }
--- chrome/data/main.js
+++ chrome/data/main.js
@@ -11,7 +11,7 @@ Authors: Joel Gascoigne         Tom Ashworth
 var config = {};
 config.plugin = {
     label: "Buffer This Page",
-    version: "2.2.4",
+    version: "2.2.5",
     guide: 'http://bufferapp.com/guides/chrome/installed',
     menu: {
         page: {
