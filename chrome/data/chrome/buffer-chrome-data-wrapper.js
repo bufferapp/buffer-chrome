37c43fab792b77a836c68692fa1cdec678414903
Tom Ashworth <tom@phuu.net>
2012-07-30 11:22:48 +0300

    Commented & improved the Chrome-specific content scripts

--- chrome/data/chrome/buffer-chrome-data-wrapper.js
+++ chrome/data/chrome/buffer-chrome-data-wrapper.js
@@ -1,11 +1,10 @@
+// Very small wrapper for getting a data URL
 var DataWrapper = function () {
-    
-    return {
-        get: function (file) {
-            return chrome.extension.getURL(file);
-        }
+  return {
+    get: function (file) {
+      return chrome.extension.getURL(file);
     }
-    
+  };
 };
 
 if(!xt) var xt = {};
--- chrome/data/chrome/buffer-chrome.js
+++ chrome/data/chrome/buffer-chrome.js
@@ -1,11 +1,14 @@
 ;(function() {
-    chrome.extension.onConnect.addListener(function(chport) {
-        
-        var overlayPort = PortWrapper(chport);
-        
-        overlayPort.on("buffer_click", function(postData) {
-            bufferData(overlayPort, postData);
-        });
-  
+  // buffer-chrome sets up the listener for "buffer_click" that
+  // makes the overlay appear after Buffer is triggered from an
+  // external source lick the browser action or a retweet in Twitter
+  chrome.extension.onConnect.addListener(function(chport) {
+    // The PortWapper creates the on/emit API for the port
+    var overlayPort = PortWrapper(chport);
+    overlayPort.on("buffer_click", function(postData) {
+      // bufferData is a method created in buffer-overlay that creates
+      // the overlay and gives it data
+      bufferData(overlayPort, postData);
     });
+  });
 }());
