73533a750a9632635e5834fc09e9572aaab96363
Philippe Masset <pioul06+github@gmail.com>
2018-02-28 19:38:10 +0100

    Show telemetry info page on update to users who haven't seen it yet

--- chrome/data/main.js
+++ chrome/data/main.js
@@ -58,6 +58,7 @@ config.plugin = {
   browser: currentBrowser,
   version: chrome.manifest.version,
   guide: 'https://buffer.com/guides/' + currentBrowser + '/installed',
+  telemetryInfo: 'https://buffer.com/guides/' + currentBrowser + '/telemetry-info',
   menu: {
     page: {
       label: "Buffer This Page"
@@ -260,9 +261,25 @@ if (chrome.runtime.onInstalled) {
           url: config.plugin.guide,
           active: true
         });
+
+        if (currentBrowser === 'firefox') {
+          // Set to "null" (the string) to remember the choice was shown, pending actual choice from user
+          localStorage.setItem('buffer.op.firefox-disable-data-collection', 'null');
+        }
       });
     } else if (details.reason == "update"){
-      // Nothing to do here, yet...
+      var hasntBeenShownTelemetryChoiceYet =
+        localStorage.getItem('buffer.op.firefox-disable-data-collection') === null;
+
+      if (currentBrowser === 'firefox' && hasntBeenShownTelemetryChoiceYet) {
+        chrome.tabs.create({
+          url: config.plugin.telemetryInfo,
+          active: true
+        });
+
+        // Set to "null" (the string) to remember the choice was shown, pending actual choice from user
+        localStorage.setItem('buffer.op.firefox-disable-data-collection', 'null');
+      }
     }
   });
 }
