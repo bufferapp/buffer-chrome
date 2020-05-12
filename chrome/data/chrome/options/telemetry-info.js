be3f88e993a7d0ddfc0df929034f6215aaeb29d8
Ana Almeida <anafilipadealmeida@gmail.com>
2020-05-12 15:22:42 +0100

    Replace jQuery ready method

--- chrome/data/chrome/options/telemetry-info.js
+++ chrome/data/chrome/options/telemetry-info.js
@@ -1,7 +1,7 @@
 // Remember that the popup was shown to user
 localStorage.setItem('buffer.op.firefox-disable-data-collection', 'null');
 
-$(document).on('ready', function() {
+$(document).ready(function() {
   $('.buttons-container button').on('click', function(e) {
     var $button = $(e.target);
     var choice = $button.attr('data-choice');
